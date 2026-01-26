e565ac9a535efb0bfd1b90e3a7987adc387abfa6
Diego Cajales <diego.a.cajales.a@gmail.com>
2026-01-26 02:56:15 +0000

    pipeline for parquet

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -12,7 +12,7 @@ from tqdm.auto import tqdm
 @click.option('--pg-host', default='localhost', help='PostgreSQL host')
 @click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
 @click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
-@click.option('--target-table', default='yellow_taxi_data_2', help='Target table name')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
 @click.option('--year', default=2025, type=int, help='Year of data to ingest')
 @click.option('--month', default=11, type=int, help='Month of data to ingest')
 def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table, year, month):
@@ -21,8 +21,6 @@ def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table, year, month):
     url = f"{prefix}/yellow_tripdata_{year:04d}-{month:02d}.parquet"
 
     engine = create_engine(f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}")
-
-    print(url)
 
     # Read parquet (no chunks)
     df = pd.read_parquet(url)
